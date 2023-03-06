a1cccfc04cc73af55520459d9f022ff8343a3517
Leonardo Ferreira de Abreu <leonardofabreu@hotmail.com>
2023-03-06 14:54:36 -0300

    Adiciona requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, 
+
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
